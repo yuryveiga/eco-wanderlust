406a2e12b7e92777a8dda85bb7e59c4f5851ca40
Syuryveiga <veiga.yury@gmail.com>
2026-04-27 09:46:46 -0300

    style: revert blog covers to fit='cover'

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -85,7 +85,7 @@ const Blog = () => {
                       alt={getTranslated(post, 'title')}
                       width={600}
                       containerClassName="w-full h-full"
-                      fit="contain"
+                      fit="cover"
                       className="w-full h-full group-hover:scale-105 transition-transform duration-500"
                       loading="lazy"
                     />
